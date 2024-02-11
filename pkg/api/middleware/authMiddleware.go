7eaf27b9e7396b841547b65cd8de53f4f00e8dc1
AsemFathigit config --global user.email Asemfathi12@gmail.cogit config --global user.name AsemFathi <Asemfathi12@gmail.com>
2024-02-11 02:54:10 +0200

    Edit bearer token in authMiddleware

--- pkg/api/middleware/authMiddleware.go
+++ pkg/api/middleware/authMiddleware.go
@@ -12,8 +12,9 @@ import (
 func Authenticate() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		clientToken := c.Request.Header.Get("Authorization")
+		hasPrefix := strings.HasPrefix(clientToken, "Bearer")
 
-		if clientToken == "" {
+		if clientToken == "" || !hasPrefix {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("No Authorization header provides")})
 			c.Abort()
 			return
